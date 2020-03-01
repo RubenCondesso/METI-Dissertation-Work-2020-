59601c7a34c90b06df0208f26f6444c750a1cc66
RubenCondesso <ruben.condesso@gmail.com>
2020-03-01 12:09:40 +0000

    Some comments on the code

--- SmartBike/Sensor_System/BLE_GATT_Server-UART_Service/uart_Communication.py
+++ SmartBike/Sensor_System/BLE_GATT_Server-UART_Service/uart_Communication.py
@@ -1,3 +1,21 @@
+#!/usr/bin/python
+#
+# uart_Communication.py - Class for the Raspberry Pi Zero
+#
+# 1 March 2020 - 1.0 
+# 
+# Autor: Ruben Condesso - 81969 - 2nd Semester (2020)
+#
+# 
+# SmartBike System - Master Thesis in Telecomunications and Computer Engineering
+#
+# 
+# Python code that send the sensor's data to the smarthphone via BLE, using a UART service 
+#
+#
+
+# -------------------------------------------------------------------------------------- Libraries ----------------------------------------------------------------------------------------- #
+
 import sys
 import dbus, dbus.mainloop.glib
 from gi.repository import GLib
@@ -5,6 +23,9 @@ from gatt_advertisement import Advertisement
 from gatt_advertisement import register_ad_cb, register_ad_error_cb
 from gatt_server import Service, Characteristic
 from gatt_server import register_app_cb, register_app_error_cb
+
+
+# ----------------------------------------------------------------------------------- Code starts here -------------------------------------------------------------------------------------- #
  
 BLUEZ_SERVICE_NAME =           'org.bluez'
 DBUS_OM_IFACE =                'org.freedesktop.DBus.ObjectManager'
@@ -16,18 +37,17 @@ UART_RX_CHARACTERISTIC_UUID =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
 UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 LOCAL_NAME =                   'RPi-Sensing_System'
 mainloop = None
+
  
 class TxCharacteristic(Characteristic):
 
-
-
     # Init the uart peripheral
     def __init__(self, bus, index, service):
+
         Characteristic.__init__(self, bus, index, UART_TX_CHARACTERISTIC_UUID,
                                 ['notify'], service)
         self.notifying = False
         GLib.io_add_watch(sys.stdin, GLib.IO_IN, self.read_SensorData)
-
 
     
     # Read the data obtained by the ultrasonic sensor
@@ -36,7 +56,6 @@ class TxCharacteristic(Characteristic):
         s = fd.readline()
 
         if s.isspace():
-            
             pass
 
         else:
@@ -55,14 +74,19 @@ class TxCharacteristic(Characteristic):
         return True
 
        
-
     # Send the distances to the obstacle, detected by the ultrasonic sensor
     def send_SensorData(self, s):
+
         if not self.notifying:
             return
+
         value = []
+        
         for c in s:
+
+            # Add the value received to the dbus
             value.append(dbus.Byte(c.encode()))
+
         self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
 
 
@@ -77,7 +101,9 @@ class TxCharacteristic(Characteristic):
             return
         self.notifying = False
  
+
 class RxCharacteristic(Characteristic):
+
     def __init__(self, bus, index, service):
         Characteristic.__init__(self, bus, index, UART_RX_CHARACTERISTIC_UUID,
                                 ['write'], service)
@@ -85,13 +111,17 @@ class RxCharacteristic(Characteristic):
     def WriteValue(self, value, options):
         print('remote: {}'.format(bytearray(value).decode()))
  
+
 class UartService(Service):
+
     def __init__(self, bus, index):
         Service.__init__(self, bus, index, UART_SERVICE_UUID, True)
         self.add_characteristic(TxCharacteristic(bus, 0, self))
         self.add_characteristic(RxCharacteristic(bus, 1, self))
  
+
 class Application(dbus.service.Object):
+
     def __init__(self, bus):
         self.path = '/'
         self.services = []
@@ -113,18 +143,26 @@ class Application(dbus.service.Object):
                 response[chrc.get_path()] = chrc.get_properties()
         return response
  
+
+# Launch the application for the uart service
 class UartApplication(Application):
+
     def __init__(self, bus):
         Application.__init__(self, bus)
         self.add_service(UartService(bus, 0))
  
+
+# Bluetooth Low Energy Advertisement
 class UartAdvertisement(Advertisement):
+
     def __init__(self, bus, index):
         Advertisement.__init__(self, bus, index, 'peripheral')
         self.add_service_uuid(UART_SERVICE_UUID)
         self.add_local_name(LOCAL_NAME)
         self.include_tx_power = True
  
+
+# Find the BLE adapter to connect
 def find_adapter(bus):
     remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
                                DBUS_OM_IFACE)
@@ -135,36 +173,59 @@ def find_adapter(bus):
         print('Skip adapter:', o)
     return None
  
+
+
+# Main function 
 def main():
+
     global mainloop
+
+    # Set the new main loop as the default for all new Connection or Bus instances
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    # Connect to the system bus
     bus = dbus.SystemBus()
+    
+    # Find the BLE adapter to get a connection
     adapter = find_adapter(bus)
+    
     if not adapter:
-        print('BLE adapter not found')
+        print(' The BLE adapter was not found.')
         return
  
+
     service_manager = dbus.Interface(
                                 bus.get_object(BLUEZ_SERVICE_NAME, adapter),
                                 GATT_MANAGER_IFACE)
+    
     ad_manager = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, adapter),
                                 LE_ADVERTISING_MANAGER_IFACE)
  
+
     app = UartApplication(bus)
     adv = UartAdvertisement(bus, 0)
- 
+    
+    # Main event loop
     mainloop = GLib.MainLoop()
- 
+    
+    # Run the application for this connection
     service_manager.RegisterApplication(app.get_path(), {},
                                         reply_handler=register_app_cb,
                                         error_handler=register_app_error_cb)
+    
+    # Advertise the Raspberry Pi Zero BLE connection
     ad_manager.RegisterAdvertisement(adv.get_path(), {},
                                      reply_handler=register_ad_cb,
                                      error_handler=register_ad_error_cb)
+    
     try:
         mainloop.run()
+
     except KeyboardInterrupt:
         adv.Release()
  
+
+
+# Main of this python code 
 if __name__ == '__main__':
     main()
