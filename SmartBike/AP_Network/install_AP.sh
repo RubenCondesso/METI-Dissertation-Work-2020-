b06ed3834152bb7401f51b69667764bbca7e4061
RubenCondesso <ruben.condesso@gmail.com>
2020-03-04 16:23:21 +0000

    AP setup finished.

--- SmartBike/AP_Network/install_AP.sh
+++ SmartBike/AP_Network/install_AP.sh
@@ -16,17 +16,17 @@
 
 clear
 
-echo "======================================================="
-echo "===== Setting up Raspberry Pi Zero Access Point ======="
-echo "======================================================="
+echo "================================================================="
+echo "========== Setting up Raspberry Pi Zero Access Point ============"
+echo "================================================================="
 
 
 
 ####################################################################################### Install dependencies  ########################################################################################
 
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install dnsmasq dhcpcd hostapd
+#sudo apt -y update
+#sudo apt -y upgrade
+#sudo apt -y install dnsmasq dhcpcd hostapd
 
 
 ############################################################################### Back up existing configuration files #################################################################################
@@ -41,6 +41,16 @@ fi
 if [ -f /etc/default/udhcpd ]
 then
 	cp /etc/default/udhcpd /etc/default/udhcpd.backup
+fi
+
+if [ -f /etc/dhcpcd.conf ]
+then
+	cp /etc/dhcpcd.conf /etc/dhcpcd.conf.backup
+fi
+
+if [ -f /etc/dnsmasq.conf ]
+then
+	cp /etc/dnsmasq.conf /etc/dnsmasq.conf.backup
 fi
 
 if [ -f /etc/network/interfaces ]
@@ -63,7 +73,6 @@ then
 	cp /etc/sysctl.conf /etc/sysctl.conf.backup
 fi
 
-
 if [ -f /etc/iptables.ipv4.nat ]
 then
 	cp /etc/iptables.ipv4.nat /etc/iptables.ipv4.nat.backup
@@ -74,7 +83,6 @@ then
 	cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.backup
 fi
 
-
 echo "Configuration files are backed up."
 
 
@@ -84,32 +92,38 @@ echo "Configuration files are backed up."
 # Configure /etc/udev/rules.d/70-persistent-net.rules
 sudo bash -c 'cat > /etc/udev/rules.d/70-persistent-net.rules' << EOF
 SUBSYSTEM=="ieee80211", ACTION=="add|change", ATTR{macaddress}=="${MAC_ADDRESS}", KERNEL=="phy0", \
-  RUN+="/sbin/iw phy phy0 interface add ap0 type __ap", \
-  RUN+="/bin/ip link set ap0 address ${MAC_ADDRESS}
+  RUN+="/sbin/iw phy phy0 interface add wlan0 type __ap", \
+  RUN+="/bin/ip link set wlan0 address ${MAC_ADDRESS}
 EOF
 
+# Configure /etc/dhcpcd.conf
+sudo bash -c 'cat > /etc/dhcpcd.conf' << EOF
+interface wlan0
+    static ip_address=192.168.0.1/24
+EOF
 
 # Configure /etc/dnsmasq.conf
 sudo bash -c 'cat > /etc/dnsmasq.conf' << EOF
-interface=wlan0
-dhcp-range=192.168.0.50,192.168.0.150,255.255.255.0,12h
-dhcp-option=3,192.168.0.1
-listen-address=127.0.0.1
+interface=wlan0      
+  dhcp-range=192.168.0.2,192.168.0.20,255.255.255.0,24h
 EOF
 
 # Configure /etc/hostapd/hostapd.conf
 sudo bash -c 'cat > /etc/hostapd/hostapd.conf' << EOF
 interface=wlan0
-hw_mode=g
-channel=6
-ieee80211n=1
-wmm_enabled=1
+driver=nl80211
 ssid=SmartBike_AP
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
 auth_algs=1
+ignore_broadcast_ssid=0
 wpa=2
+wpa_passphrase=1234567890
 wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
 rsn_pairwise=CCMP
-wpa_passphrase=123456789
 EOF
 
 # Configure /etc/wpa_supplicant/wpa_supplicant.conf
@@ -124,27 +138,14 @@ sudo bash -c 'cat > /etc/default/hostapd' << EOF
 DAEMON_CONF="/etc/hostapd/hostapd.conf"
 EOF
 
-# Configure /etc/network/interfaces
-sudo bash -c 'cat > /etc/network/interfaces' << EOF
-source-directory /etc/network/interfaces.d
-auto lo
-iface lo inet loopback
-
-auto wlan0
-iface wlan0 inet static
-    address 192.168.10.1
-    netmask 255.255.255.0
-    hostapd /etc/hostapd/hostapd.conf
-EOF
-
 # Configure /bin/launch_AP.sh
 sudo bash -c 'cat > /bin/launch_AP.sh' << EOF
 echo 'Starting Access Point.'
-sleep 30
+sleep 50
 sudo ifdown --force wlan0
 sudo ifup wlan0
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s 192.168.10.0/24 ! -d 192.168.10.0/24 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 192.168.0.0/24 ! -d 192.168.0.0/24 -j MASQUERADE
 sudo systemctl restart dnsmasq
 EOF
 
