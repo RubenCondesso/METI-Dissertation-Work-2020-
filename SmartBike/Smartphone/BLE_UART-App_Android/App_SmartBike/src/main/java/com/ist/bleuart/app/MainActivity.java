5c92cfb671500e497989daaf39cf73fb7276a018
RubenCondesso <ruben.condesso@gmail.com>
2020-03-23 15:06:54 +0000

    Update on comments on code

--- SmartBike/Smartphone/BLE_UART-App_Android/App_SmartBike/src/main/java/com/ist/bleuart/app/MainActivity.java
+++ SmartBike/Smartphone/BLE_UART-App_Android/App_SmartBike/src/main/java/com/ist/bleuart/app/MainActivity.java
@@ -1,3 +1,30 @@
+
+/*
+#
+# MainActivity.java - Java Class of Android App
+#
+# 23 March 2020 - 1.0 
+# 
+# Autor: Ruben Condesso - 81969 - 2nd Semester (2020)
+#
+# 
+# SmartBike System - Master Thesis in Telecomunications and Computer Engineering
+#
+# 
+# Java class that sends and receives data to a Bluetooth Low Energy UART service, which is running on the Raspberry Pi Zero (BLE GATT Server)
+#
+# */ 
+
+
+/*
+# =================================================================================== Code starts here =====================================================================================#
+*/ 
+
+
+/*
+# -------------------------------------------------------------------------------------- Libraries ----------------------------------------------------------------------------------------- #
+*/ 
+
 package com.ist.bleuart.app;
 
 import android.app.Activity;
@@ -22,134 +49,161 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/*
+# -------------------------------------------------------------------------------------- Functions ------------------------------------------------------------------------------------------ #
+*/
+
 public class MainActivity extends Activity {
 
-    // UUIDs for UAT service and associated characteristics.
+    // UUIDs for UAT service and associated characteristics
     public static UUID UART_UUID = UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
     public static UUID TX_UUID = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
     public static UUID RX_UUID = UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
-    // UUID for the BTLE client characteristic which is necessary for notifications.
+
+    // UUID for the BLE client characteristic -> necessary for notifications
     public static UUID CLIENT_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
     // UI elements
     private TextView messages;
     private EditText input;
 
-    // BTLE state
+    // BLE state
     private BluetoothAdapter adapter;
     private BluetoothGatt gatt;
     private BluetoothGattCharacteristic tx;
     private BluetoothGattCharacteristic rx;
 
-    // Main BTLE device callback where much of the logic occurs.
+    // BLE device callbacks -> Handles the main logic of this class
     private BluetoothGattCallback callback = new BluetoothGattCallback() {
-        // Called whenever the device connection state changes, i.e. from disconnected to connected.
+
+        // Function called whenever the device connection state changes -> from disconnected state to connected state
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+
             super.onConnectionStateChange(gatt, status, newState);
+
+            // Different states that can exist or change to
+
             if (newState == BluetoothGatt.STATE_CONNECTED) {
-                writeLine("Connected!");
-                // Discover services.
+                writeLine("Connected to GATT Server - Sensing System");
+
                 if (!gatt.discoverServices()) {
                     writeLine("Failed to start discovering services!");
                 }
             }
             else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
-                writeLine("Disconnected!");
+                writeLine("Disconnected from GATT Server");
             }
             else {
                 writeLine("Connection state changed.  New state: " + newState);
             }
         }
 
-        // Called when services have been discovered on the remote device.
-        // It seems to be necessary to wait for this discovery to occur before
-        // manipulating any services or characteristics.
+        // Function called when service have been discovered on the remote device (Raspberry Pi Zero)
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+
             super.onServicesDiscovered(gatt, status);
+
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                writeLine("Service discovery completed!");
+                writeLine("Sensing System Service discovery completed.");
             }
             else {
-                writeLine("Service discovery failed with status: " + status);
+                writeLine("Sensing System Service discovery failed with status: " + status);
             }
-            // Save reference to each characteristic.
+
+            // Save reference to each characteristic -> Read and Write
             tx = gatt.getService(UART_UUID).getCharacteristic(TX_UUID);
             rx = gatt.getService(UART_UUID).getCharacteristic(RX_UUID);
-            // Setup notifications on RX characteristic changes (i.e. data received).
-            // First call setCharacteristicNotification to enable notification.
+
+            // Setup notifications on RX characteristic changes -> Data received
+
+            // (first) Call setCharacteristicNotification to enable notification
             if (!gatt.setCharacteristicNotification(rx, true)) {
-                writeLine("Couldn't set notifications for RX characteristic!");
+                writeLine("Could not set notifications for RX characteristic");
             }
-            // Next update the RX characteristic's client descriptor to enable notifications.
+
+            // (next) Update the RX characteristic's client descriptor to enable notifications
             if (rx.getDescriptor(CLIENT_UUID) != null) {
                 BluetoothGattDescriptor desc = rx.getDescriptor(CLIENT_UUID);
                 desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
                 if (!gatt.writeDescriptor(desc)) {
-                    writeLine("Couldn't write RX client descriptor value!");
+                    writeLine("Could not write RX client descriptor value");
                 }
             }
             else {
-                writeLine("Couldn't get RX client descriptor!");
+                writeLine("Could not get RX client descriptor");
             }
         }
 
-        // Called when a remote characteristic changes (like the RX characteristic).
+        // Called when a remote characteristic changes -> like the RX characteristic
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+
             super.onCharacteristicChanged(gatt, characteristic);
             writeLine("Received: " + characteristic.getStringValue(0));
         }
     };
 
-    // BTLE device scanning callback.
+    // BLE device scanning callback
     private LeScanCallback scanCallback = new LeScanCallback() {
-        // Called when a device is found.
+
+        // Funcction called when the GATT Server (RPi Zero) is found
         @Override
         public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+
             writeLine("Found device: " + bluetoothDevice.getAddress());
-            // Check if the device has the UART service.
+
+            // Check if the device has the UART service
             if (parseUUIDs(bytes).contains(UART_UUID)) {
-                // Found a device, stop the scan.
+
+                // Found the device -> stop the scanning
                 adapter.stopLeScan(scanCallback);
-                writeLine("Found UART service!");
-                // Connect to the device.
-                // Control flow will now go to the callback functions when BTLE events occur.
+
+                writeLine("Found the UART service");
+
+                // Connect to the GATT Server - Raspberry Pi Zero
+                // Control flow will now go to the callback functions when BLE events occur
                 gatt = bluetoothDevice.connectGatt(getApplicationContext(), false, callback, 2);
             }
         }
     };
 
-    // OnCreate, called once to initialize the activity.
+    // OnCreate -> called once to initialize the activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Grab references to UI elements.
+        // Grab the references to the UI elements
         messages = (TextView) findViewById(R.id.messages);
         input = (EditText) findViewById(R.id.input);
 
         adapter = BluetoothAdapter.getDefaultAdapter();
     }
 
-    // OnResume, called right before UI is displayed.  Start the BTLE connection.
+    // OnResume -> called right before UI is displayed.
+    // Start the BLE connection
     @Override
     protected void onResume() {
+
         super.onResume();
-        // Scan for all BTLE devices.
-        // The first one with the UART service will be chosen--see the code in the scanCallback.
+
+        // Scan for all BLE devices
+        // The first one with the UART service (RPi Zero) will be chosen 
         writeLine("Scanning for devices...");
         adapter.startLeScan(scanCallback);
     }
 
-    // OnStop, called right before the activity loses foreground focus.  Close the BTLE connection.
+    // OnStop _> called right before the activity loses foreground focus
+    // Close the BTLE connection
     @Override
     protected void onStop() {
+
         super.onStop();
         if (gatt != null) {
-            // For better reliability be careful to disconnect and close the connection.
+
             gatt.disconnect();
             gatt.close();
             gatt = null;
@@ -158,26 +212,28 @@ public class MainActivity extends Activity {
         }
     }
 
-    // Handler for mouse click on the send button.
+    // Handler for click on the send button
     public void sendClick(View view) {
+
         String message = input.getText().toString();
         if (tx == null || message == null || message.isEmpty()) {
-            // Do nothing if there is no device or message to send.
+            // Do nothing if there is no device or message to send
             return;
         }
-        // Update TX characteristic value.  Note the setValue overload that takes a byte array must be used.
+
+        // Update TX characteristic value
+        // Note the setValue overload that takes a byte array must be used
         tx.setValue(message.getBytes(Charset.forName("UTF-8")));
         if (gatt.writeCharacteristic(tx)) {
-            writeLine("Sent: " + message);
+            writeLine("Sent from App: " + message);
         }
         else {
-            writeLine("Couldn't write TX characteristic!");
+            writeLine("Could not write TX characteristic.");
         }
     }
 
-    // Write some text to the messages text view.
-    // Care is taken to do this on the main UI thread so writeLine can be called
-    // from any thread (like the BTLE callback).
+    // Write data to the messages text view
+    // Care is taken to do this on the main UI thread so writeLine can be called from any thread (like the BLE callback)
     private void writeLine(final CharSequence text) {
         runOnUiThread(new Runnable() {
             @Override
@@ -188,9 +244,7 @@ public class MainActivity extends Activity {
         });
     }
 
-    // Filtering by custom UUID is broken in Android 4.3 and 4.4, see:
-    //   http://stackoverflow.com/questions/18019161/startlescan-with-128-bit-uuids-doesnt-work-on-native-android-ble-implementation?noredirect=1#comment27879874_18019161
-    // This is a workaround function from the SO thread to manually parse advertisement data.
+    // Workaround function from the SO thread to manually parse advertisement data
     private List<UUID> parseUUIDs(final byte[] advertisedData) {
         List<UUID> uuids = new ArrayList<UUID>();
 
@@ -213,6 +267,7 @@ public class MainActivity extends Activity {
                     break;
                 case 0x06:// Partial list of 128-bit UUIDs
                 case 0x07:// Complete list of 128-bit UUIDs
+
                     // Loop through the advertised 128-bit UUID's.
                     while (len >= 16) {
                         try {
@@ -223,7 +278,7 @@ public class MainActivity extends Activity {
                             uuids.add(new UUID(leastSignificantBit,
                                     mostSignificantBit));
                         } catch (IndexOutOfBoundsException e) {
-                            // Defensive programming.
+                            // Defensive programming
                             //Log.e(LOG_TAG, e.toString());
                             continue;
                         } finally {
@@ -241,21 +296,20 @@ public class MainActivity extends Activity {
         return uuids;
     }
 
-    // Boilerplate code from the activity creation:
-
+    // Boilerplate code from the activity creation
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu -> This adds items to the action bar if it is present
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Handle action bar item clicks here
+        // The action bar will automatically handle clicks on the Home/Up button, so long as its be specify a parent activity in AndroidManifest.xml
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
